a4e490064906b963bc74351e7c3da70530e5e577
Anubhav Srivastava <2017pcecsanubhav027@poornima.org>
2020-05-22 22:16:28 +0530

    all files added

---
+++ Cat Generator/static/home.js
@@ -0,0 +1,6 @@
+function gen_cat(){
+    var image = document.createElement('img');
+    var d = document.getElementById('flex-cat-gen');
+    image.src = "static/cat.gif";
+    d.appendChild(image);
+}
